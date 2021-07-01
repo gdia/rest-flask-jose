37e131bfbca365f61c4c9def56a5472c306f0b81
gdia-mt <diamantis.g1@gmail.com>
2021-07-01 17:18:12 +0300

    Uppdate

--- blog/tests/post_test.py
+++ blog/tests/post_test.py
@@ -4,4 +4,5 @@ from post import Post
 class PostTest(TestCase):
     def test_create_post(self):
         p = Post('Test','Abracadabra')
-        
+        self.assertEquals('Test',p.title)
+        self.assertEquals('Abracadabra',p.content)
